c74eb5eacff7819792706b1a0f5d091be2e5c25f
Flazened <ytmichaelyusliardy88@gmail.com>
2026-07-21 08:30:46 +0700

    feats: add something (check it and read the code by your self kids)

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+class StudentController extends Controller
+{
+    public function index()
+    
+    {
+        return 'ini adalah halaman daftar siswa';
+    }
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::name('students.')->prefix('students')->group(function(){
+Route::get('/', [StudentController::class, 'index'])->name('index');
+
+Route::get('/', function () {
+    return "Goat Game PC junior";
+})->name('students.index');
+
+Route::get('/{id}', function ($id) {
+    return "Menampilakn id:{$id}";
+})->name('students.show');
+});
+
